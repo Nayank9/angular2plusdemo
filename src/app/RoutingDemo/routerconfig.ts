b9f435312ad8f9ff06411db744eb2c4b7e86e959
Nayank9 <nkagade@gmail.com>
2019-04-26 09:32:40 -0500

    2 files modified

--- src/app/RoutingDemo/routerconfig.ts
+++ src/app/RoutingDemo/routerconfig.ts
@@ -2,6 +2,6 @@ import {Routes} from '@angular/router';
 import {AdminComponent} from './admin.component';
 import {StudentComponent} from './student.component';
 export const myOwnRouter:Routes=[
-  {path:'',compoent:AdminComponent},
-  {path:'student',compoent:StudentComponent}
+  {path:'',component:AdminComponent},
+  {path:'student',component:StudentComponent}
 ]
